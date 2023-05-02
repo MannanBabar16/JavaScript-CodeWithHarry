086c5afbddbb30e1d59d91d1a9fc5c34cac15001
MannanBabar16 <mannan.babar16@gmail.com>
2023-05-02 15:13:30 +0500

    chapter1 complete

--- ch1.js
+++ ch1.js
@@ -1,5 +1,64 @@
+//#02 Variables & Data
+console.log("\n#02")
 let a = 10;
 console.log(a);
 
-a = 'TopGg'; //a="TopG"
+a = 'TopG'; //a="TopG"
 console.log(a);
+
+
+
+//#03 Let, Var, Const
+console.log("\n#03")
+let b="Topg";
+const author="harry";
+b=4;
+let c=null;
+let d=undefined;
+{
+    let b='this'
+    console.log(b)
+
+}
+console.log(b);
+
+
+
+//#04 Primitives data types (NN DB SS U) & ojects
+
+//primitive
+console.log("\n#04")
+let aa=null;
+let bb=345;
+let cc=true;
+let dd=BigInt("567");
+let ee="TopG";
+let ff=Symbol("I am a nice symbol");
+let gg=undefined;
+
+//printing all
+console.log(aa,bb,cc,dd,ee,ff,gg);
+
+//printing data type of dd
+console.log(typeof dd);
+
+
+//Objects/Non primitive
+const item={
+    "Andrew":true,
+    "Tristan":false,
+    "Luc":67,
+    "Gorgiana":undefined
+}
+
+console.log(item);
+console.log(item["Andrew"]);
+
+
+//Quick Quiz
+const Student={
+    name:"TopG",
+    phoneNumber:123456,
+    marks:100,
+}
+console.log("Quick Quiz: \n",Student);
